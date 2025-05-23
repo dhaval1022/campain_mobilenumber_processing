6fce9b8f9d72eafc7225968ac6f49f049cb32892
Dhaval <ds@medkart.in>
2025-05-23 09:38:16 +0530

    Refactor .gitignore, update database secrets access, and clean up requirements.txt

--- connections.py
+++ connections.py
@@ -1,14 +1,12 @@
-import os
 import psycopg2
-import logging
 import streamlit as st
 
 
-DB_POS_HOST = st.secrets["DB_POS_HOST"]
-DB_POS_PORT = st.secrets["DB_POS_PORT"]
-DB_POS_NAME = st.secrets["DB_POS_NAME"]
-DB_POS_USER = st.secrets["DB_POS_USER"]
-DB_POS_PASS = st.secrets["DB_POS_PASS"]
+DB_POS_HOST = st.secrets['database']["DB_POS_HOST"]
+DB_POS_PORT = st.secrets['database']["DB_POS_PORT"]
+DB_POS_NAME = st.secrets['database']["DB_POS_NAME"]
+DB_POS_USER = st.secrets['database']["DB_POS_USER"]
+DB_POS_PASS = st.secrets['database']["DB_POS_PASS"]
 
 def connection_pos():
     HOST = DB_POS_HOST
